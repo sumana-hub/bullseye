0ff3c3f48b0befadc2bfdf1a175ac73b1e648357
Sumana Dhital <sumanadhi17@gmail.com>
2024-05-22 17:55:46 -0400

    Added Button

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -14,6 +14,20 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showAlert() {
+      let alert = UIAlertController(
+        title: "Hello, World",
+        message: "This is my first app!",
+        preferredStyle: .alert)
+
+      let action = UIAlertAction(
+        title: "Awesome",
+        style: .default,
+        handler: nil)
+
+      alert.addAction(action)
+      present(alert, animated: true, completion: nil)
+    }
 
 }
 
