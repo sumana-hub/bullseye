088d4c7f79da30b12c84f8191d2384c04af50016
sumana-hub <sumana.dhital@triosstudent.com>
2024-05-23 11:27:02 -0400

    End of Chapter 5

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -9,24 +9,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var targetLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var roundLabel: UILabel!
+
+    var currentValue: Int = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
+
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      super.viewDidLoad()
+      startNewRound()  // Replace previous code with this
     }
 
+
     @IBAction func showAlert() {
-      let alert = UIAlertController(
+        let difference = abs(targetValue - currentValue)
+        let points = 100 - difference
+        score += points
+
+        let message = "You scored \(points) points"
+
+        let alert = UIAlertController(
         title: "Hello, World",
-        message: "This is my first app!",
+        message: message,    // changed
         preferredStyle: .alert)
 
-      let action = UIAlertAction(
-        title: "Awesome",
+        let action = UIAlertAction(
+        title: "OK",          // changed
         style: .default,
         handler: nil)
 
-      alert.addAction(action)
+        alert.addAction(action)
+
       present(alert, animated: true, completion: nil)
+        
+      startNewRound()
+    }
+
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+      currentValue = lroundf(slider.value)
+    }
+
+    func startNewRound() {
+        round += 1 
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()  // Add this line
+    }
+
+    func updateLabels() {
+      targetLabel.text = String(targetValue)
+      scoreLabel.text = String(score)
+      roundLabel.text = String(round)
     }
 
     
