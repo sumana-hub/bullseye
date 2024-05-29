e6d62b7572e5b41020edfd38123f230848a136d5
sumana-hub <sumana.dhital@triosstudent.com>
2024-05-29 13:05:21 -0400

    End of Chapter 7

--- BullsEye/BullsEye/ViewController.swift
+++ BullsEye/BullsEye/ViewController.swift
@@ -22,6 +22,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
       super.viewDidLoad()
       startNewGame()        // this line changed
+        
+      let thumbImageNormal = UIImage(named: "SliderThumb-Normal")!
+      slider.setThumbImage(thumbImageNormal, for: .normal)
+
+      let thumbImageHighlighted = UIImage(
+        named: "SliderThumb-Highlighted")!
+        slider.setThumbImage(thumbImageHighlighted, for: .highlighted)
+
+      let insets = UIEdgeInsets(
+          top: 0,
+          left: 14,
+          bottom: 0,
+          right: 14)
+
+      let trackLeftImage = UIImage(named: "SliderTrackLeft")!
+      let trackLeftResizable = trackLeftImage.resizableImage(
+          withCapInsets: insets)
+        slider.setMinimumTrackImage(trackLeftResizable, for: .normal)
+
+      let trackRightImage = UIImage(named: "SliderTrackRight")!
+      let trackRightResizable = trackRightImage.resizableImage(
+          withCapInsets: insets)
+        slider.setMaximumTrackImage(trackRightResizable, for: .normal)
+
     }
 
     @IBAction func showAlert() {
